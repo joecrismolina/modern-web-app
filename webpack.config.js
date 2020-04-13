bcac8074d362dbdff33fcdec90ada49c914fc2bf
joecris <joecris.molina@gmail.com>
2020-04-13 19:22:24 +1000

    added initial webpack and babel setups. edited documentation

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/bundle.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    },
+    devServer: {
+        contentBase: './dist'
+    }
+}
